8eb2e0af5ddd5c6b12e3a814dbd98ce843ab37ab
noukpoherve <noukpoherve@users.noreply.github.com>
2026-08-24 21:50:27 +0000

    docs(preview): storybook PR 63 fa0ac63599f12e00f75b8f591c22abfb8b28d655

--- prs/63/src/index.d.ts
+++ prs/63/src/index.d.ts
@@ -1,5 +1,7 @@
 export { Button } from './components/Button/Button';
-export type { ButtonProps, ButtonSize, ButtonVariant } from './components/Button/Button';
+export type { ButtonIconPosition, ButtonProps, ButtonSize, ButtonVariant, } from './components/Button/Button';
+export { IconButton } from './components/Button/IconButton';
+export type { IconButtonProps } from './components/Button/IconButton';
 export { ThemeProvider } from './theme/ThemeProvider';
 export type { ThemeMode, ThemeProviderProps } from './theme/ThemeProvider';
 export { VisuallyHidden } from './a11y/VisuallyHidden';
