e9a80c8be51ed42f6fd23874e647aab1f23ce8d1
fruitizz <fruitizz@users.noreply.github.com>
2026-08-28 11:20:45 +0000

    docs(preview): storybook PR 89 80b2c6715bda5d634e9dacc2878cf228ecf3002d

---
+++ prs/89/assets/Link-ByLzs2Bc.js
@@ -0,0 +1,28 @@
+import{u as l,j as n,M as j,P as o,C as e,a as t,b as r,U as x,A as c}from"./iframe-dNC5VVjK.js";import{L as a,D as m,W as d,a as h,b as f,I as p,E as u}from"./Link.stories-BTpLZwgn.js";import"./preload-helper-Dp1pzeXC.js";import"./arg-types-DazVfg0r.js";import"./docs-source-C_O40UCi.js";import"./Icon-BYPTyP4z.js";import"./Text-D8GEkm7p.js";import"./VisuallyHidden-jlN6IZiu.js";function k(s){const i={h1:"h1",...l(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(j,{of:a}),`
+`,n.jsx(i.h1,{id:"link",children:"Link"}),`
+`,n.jsx(o,{k:"link.intro"}),`
+`,n.jsx(o,{k:"chrome.examplesFollow"}),`
+`,n.jsx(e,{of:m}),`
+`,n.jsx(t,{k:"chrome.icon"}),`
+`,n.jsx(o,{k:"link.iconBody"}),`
+`,n.jsx(e,{of:d}),`
+`,n.jsx(t,{k:"link.color"}),`
+`,n.jsx(o,{k:"link.colorBody"}),`
+`,n.jsx(e,{of:h}),`
+`,n.jsx(e,{of:f}),`
+`,n.jsx(t,{k:"link.inText"}),`
+`,n.jsx(o,{k:"link.inTextBody"}),`
+`,n.jsx(e,{of:p}),`
+`,n.jsx(t,{k:"link.external"}),`
+`,n.jsx(o,{k:"link.externalBody"}),`
+`,n.jsx(e,{of:u}),`
+`,n.jsx(o,{k:"chrome.themeLang"}),`
+`,n.jsx(t,{k:"chrome.accessibility"}),`
+`,n.jsx(o,{k:"link.a11yBody"}),`
+`,n.jsx(r,{k:"chrome.do"}),`
+`,n.jsx(x,{items:["link.doHref","link.doExternal","link.doIcon"]}),`
+`,n.jsx(r,{k:"chrome.dont"}),`
+`,n.jsx(x,{items:["link.dontBlank","link.dontButton","link.dontDarkInText"]}),`
+`,n.jsx(t,{k:"chrome.properties"}),`
+`,n.jsx(o,{k:"link.props"}),`
+`,n.jsx(c,{of:a,include:["href","color","icon","iconPosition","external","externalLabel"]})]})}function T(s={}){const{wrapper:i}={...l(),...s.components};return i?n.jsx(i,{...s,children:n.jsx(k,{...s})}):k(s)}export{T as default};
---
+++ prs/89/assets/formatter-L2YTDE3Q-BcssiE5W.js
@@ -0,0 +1,1 @@
+import{p as i,q as a,w as m}from"./iframe-dNC5VVjK.js";import"./preload-helper-Dp1pzeXC.js";var t=a(m(),1),p=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{p as formatter};
